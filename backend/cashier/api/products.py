7565f3537c4d3de294f5a28f7cb5fac41119a5e6
Sam Vervaeck <samvv@pm.me>
2025-12-06 19:21:50 +0100

    Create ProductView to replace ProductPublic

--- backend/cashier/api/products.py
+++ backend/cashier/api/products.py
@@ -1,18 +1,35 @@
+from collections.abc import Sequence
+from decimal import Decimal
 from fastapi import APIRouter, Depends
+from pydantic import UUID4, BaseModel
 from sqlmodel import Session
+
 
 from cashier.dependencies import get_session
 import cashier.crud as crud
-from cashier.models.products import ProductPublic
+from cashier.models import Product
+
 
 router = APIRouter(tags=["products"], prefix="/products")
 
 
-@router.get('/', response_model=list[ProductPublic])
-def get_products(*, session: Session = Depends(get_session)) -> list[Product]:
+class ProductView(BaseModel):
+    id: UUID4
+    ean: str | None
+    price: Decimal
+
+
+def render_product(product: Product) -> ProductView:
     """
-    Returns a list of all products
-    :return:
+    Render only those fields that the user is allowed to see.
     """
-    products = crud.list_products(session)
-    return products
+    return ProductView(
+        id=product.id,
+        ean=product.ean,
+        price=product.price,
+    )
+
+
+@router.get('/')
+async def get_products(*, session: Session = Depends(get_session)) -> Sequence[ProductView]:
+    return [ render_product(product) for product in crud.list_products(session) ]
