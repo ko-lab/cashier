156a56d71e6ed65f307b7e30ed656f2dc8555990
Sam Vervaeck <samvv@pm.me>
2025-12-06 19:25:34 +0100

    Add missing `build_engine` and fix constant reference

--- backend/cashier/core.py
+++ backend/cashier/core.py
@@ -1,5 +1,11 @@
 from sqlmodel import create_engine
 
-from cashier.constants import DB_URI, DEVELOPMENT
+from cashier.constants import IS_DEBUG
 
-engine = create_engine(DB_URI, echo=DEVELOPMENT)
+def build_engine(uri: str):
+    return create_engine(
+        uri,
+        echo=IS_DEBUG,
+        connect_args={ "check_same_thread": False }
+    )
+
